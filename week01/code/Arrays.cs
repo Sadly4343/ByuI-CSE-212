22f1d97b42ee07b6bbe0126d49276a47f4d4df69
Sadly4343 <loganhartshorn@gmail.com>
2024-11-03 20:46:20 -0700

    co

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,6 @@
+using System.Globalization;
+using System.Numerics;
+
 public static class Arrays
 {
     /// <summary>
@@ -12,8 +15,32 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        //Create an array to store the new variables that will be multiples of whatever the number is.
+        //We assume length is greater than 0
+        // possibly a while loop to say while count is not equal to length we add a value to the number each time.
+        //Or we can do a for loop and say for each count add a value and multiply that together with the number so it adds on.
+        //Append the new number to the array at the end with Add.
+        //Display the array console.writeline();
+        List<double> multiples = new List<double>();
+        int count = 0;
+        for (int i = 0; i < length; i++)
+        {
+            count++;
+            if (count != 0)
+            {
+                double total = number * count;
+                multiples.Add(total);
 
-        return []; // replace this return statement with your own
+            }
+
+        }
+        double[] totals = multiples.ToArray();
+
+
+
+
+
+        return totals; // replace this return statement with your own
     }
 
     /// <summary>
