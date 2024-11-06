7895653b4132951be375847c2fa4025db8234cb4
Sadly4343 <loganhartshorn@gmail.com>
2024-11-06 02:31:11 -0700

    co

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -11,16 +11,23 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+
+        //We assume length is greater than 0
+        // possibly a while loop to say while count is not equal to length we add a value to the number each time.
+        //Or we can do a for loop and say for each count add a value and multiply that together with the number so it adds on.
+        //Append the new number to the array at the end with Add.
+        //Create a list to be used for storing the numbers.
+        //Need to initialize a variable to keep track of the current count of the loop.
+        //Create an if statement that will compute the calculation if count is not 0;
+        //Perform the calculation based on the number and multiply it by the current count.
+        //Add to the list each time the for loop until length is met.
+        //Convert the list to an array and return the new array at the end.
         //Create an array to store the new variables that will be multiples of whatever the number is.
         //We assume length is greater than 0
         // possibly a while loop to say while count is not equal to length we add a value to the number each time.
         //Or we can do a for loop and say for each count add a value and multiply that together with the number so it adds on.
         //Append the new number to the array at the end with Add.
-        //Display the array console.writeline();
+        //
         List<double> multiples = new List<double>();
         int count = 0;
         for (int i = 0; i < length; i++)
@@ -56,5 +63,22 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        //The loop can only run for the specified amount given in the method and will allow for usability it will also not return any values.
+        //For the amount given we create a for loop and delete the value at the last size of the list 
+        //Need to give a value to the final value that can be changed each time the for loop runs to stay with the count.
+        // we can get this by using count to figure out how large the list is and after we can remove the items from the list
+        //after we remove we will insert at 0 in the list.
+        //This should make the list dynamically adapt to any size list and follow the amount as how many times we will do thi
+        for (int i = 0; i < amount; i++)
+        {
+            int last = data[data.Count - 1];
+
+            data.RemoveAt(data.Count - 1);
+
+            data.Insert(0, last);
+
+        }
+
     }
 }
+
